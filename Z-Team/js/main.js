36d23b3751bcbf5432ddcee1b1e81ff712cac17e
anh tu <levantu.nd1997@gmail.com>
2019-07-05 08:13:28 +0700

    update z-team

--- Z-Team/js/main.js
+++ Z-Team/js/main.js
@@ -10,7 +10,36 @@ heightSlide();
 
 // Tooltip
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+// Testimonials
+$('.js-testimonials').slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 1,
+  arrows: false,
+  slidesToScroll: 1,
+  autoplay: true,
+  speed: 300
+});
+
+// Counter
+$('.js-counter-pro').counterUp({
+  delay: 20,
+  time: 1000
+});
+$('.js-counter-hours').counterUp({
+  delay: 20,
+  time: 1000
+});
+$('.js-counter-feedback').counterUp({
+  delay: 20,
+  time: 1000
+});
+$('.js-counter-client').counterUp({
+  delay: 20,
+  time: 1000
+});
 
 });
